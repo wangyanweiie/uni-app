5dc270a8d3c8a4ff94871b78d2f4f1785f098d1c
wyw <740484522@qq.com>
2023-08-02 10:35:19 +0800

    perf: 测试校验

--- .eslintignore.js
+++ .eslintignore.js
@@ -1,3 +1,3 @@
-dist;
 public;
+dist;
 node_modules;
--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,21 @@
 module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
     parser: 'vue-eslint-parser',
-    extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended', 'standard'],
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+        '@vue/typescript/recommended',
+    ],
     parserOptions: {
         parser: '@typescript-eslint/parser',
         ecmaVersion: 2020,
         sourceType: 'module',
     },
-    plugins: ['vue', '@typescript-eslint'],
     rules: {
         'prettier/prettier': 'error',
         'vue/component-name-in-template-casing': ['error', 'kebab-case'],
