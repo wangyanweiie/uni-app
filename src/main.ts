67e25d27d68d59b08bcb684cabed2e860c78710e
wyw <740484522@qq.com>
2023-09-07 17:04:33 +0800

    ci(uni-app): 异步注册组件

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
-import {
-    createSSRApp,
-    // defineAsyncComponent
-} from 'vue';
+import { createSSRApp, defineAsyncComponent } from 'vue';
 import App from './App.vue';
 import uviewPlus from 'uview-plus';
 import { components } from '@/components/register';
-// import { handleToHumpFormat } from '@/utils/hooks';
+import { handleToHumpFormat } from '@/utils/hooks';
 
 export function createApp() {
     const app = createSSRApp(App);
@@ -13,19 +10,18 @@ export function createApp() {
     app.use(uviewPlus);
 
     // 通过声明组件 name 注册组件
-    components.map(item => {
-        app.component(item.name, item);
-    });
+    // components.map(item => {
+    //     app.component(item.name, item);
+    // });
 
     // 通过 defineAsyncComponent 异步注册指定路径下的组件
-    // const components = import.meta.glob('./components/*/index.vue');
+    const components = import.meta.glob('./components/*/index.vue');
 
-    // for (const [key, component] of Object.entries(components)) {
-    //     const name = key.split('/')[2];
-    //     const componentName = handleToHumpFormat(name, 'max');
-
-    //     app.component(componentName, defineAsyncComponent(component as any));
-    // }
+    for (const [key, component] of Object.entries(components)) {
+        const name = key.split('/')[2];
+        const componentName = handleToHumpFormat(name, 'max');
+        app.component(componentName, defineAsyncComponent(component as any));
+    }
 
     return {
         app,
